ad99cc42d3a47e0de11c512e70724d80a9562f70
alpauna <alpauna@gmail.com>
2024-12-13 04:18:45 +0000

    static case works strange with negative numbers.

--- esphome/components/hx710/hx710.cpp
+++ esphome/components/hx710/hx710.cpp
@@ -88,7 +88,7 @@ float HX710Sensor::sample() {
       }
     } else {
       ESP_LOGD(TAG, "'%s': As RAW Value because 0.0 ref voltage %" PRId32, this->name_.c_str(), value);
-      return static_cast<float>(result);
+      return (float) result;
     }
   }
   return static_cast<float>(result);
