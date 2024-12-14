178ee782edd4486a4a746fe1162122b14a61335c
alpauna <alpauna@gmail.com>
2024-12-14 02:45:04 +0000

    Still messing with result as negative

--- esphome/components/hx710/hx710.cpp
+++ esphome/components/hx710/hx710.cpp
@@ -88,7 +88,13 @@ float HX710Sensor::sample() {
       }
     } else {
       ESP_LOGD(TAG, "'%s': As RAW value because 0.0 ref voltage %" PRId32, this->name_.c_str(), result);
-      return static_cast<float>(result);
+      if (result == NAN) {
+        int32_t res = static_cast<int32_t>(result);
+        if (res < 0) {
+          return static_cast<int32_t>(-abs(res));
+        }
+      }
+      return result;
     }
   }
   return result;
